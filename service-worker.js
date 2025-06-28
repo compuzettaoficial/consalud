55a816c4128db392172c4a13379dad27824fdc3b
consalud <compuzettaoficial@gmail.com>
2025-06-27 23:13:19 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,17 @@
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open('recetas-cache').then(cache => cache.addAll(['/', '/index.html']))
+    caches.open('recetas-cache').then(cache => cache.addAll([
+      '/',
+      '/index.html',
+      '/style.css',
+      '/app.js',
+      '/firebase-config.js',
+      '/manifest.json'
+    ]))
   );
 });
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
+    caches.match(e.request).then(res => res || fetch(e.request))
   );
 });
