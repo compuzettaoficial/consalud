9430c9cd8b3031e7f9d70578c421424422fef5ec
consalud <compuzettaoficial@gmail.com>
2025-06-27 21:19:01 -0500

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBcJ4tbucMLF3lTuDne5cyin4oyoVhTfSs",
+  authDomain: "consalud-recetas.firebaseapp.com",
+  projectId: "consalud-recetas",
+  storageBucket: "consalud-recetas.appspot.com",
+  messagingSenderId: "477690744464",
+  appId: "1:477690744464:web:597172e85fd29549fd9215"
+};
+firebase.initializeApp(firebaseConfig);
